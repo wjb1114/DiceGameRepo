4f18286e562a0eb77f035e8dad80a84a51f29e11
wjb1114 <kbd1114@gmail.com>
2019-06-21 16:34:15 -0500

    Added base HTML and JS files. Added logic for building game window and generating inputs for starting a new day.

---
+++ diceGame.js
@@ -0,0 +1,84 @@
+"use strict";
+// currentLevel, currentXP, str, acc, hp, def, unlockedAreas, dayCounter
+let defaultVisitedZones = ["Home"];
+let defaultNewPlayerArgs = new Map();
+defaultNewPlayerArgs.set("currentLevel", 1);
+defaultNewPlayerArgs.set("currentXP", 0);
+defaultNewPlayerArgs.set("currentStrength", 10);
+defaultNewPlayerArgs.set("currentAccuracy", 20);
+defaultNewPlayerArgs.set("currentMaxHealth", 1000);
+defaultNewPlayerArgs.set("currentDefense", 20);
+defaultNewPlayerArgs.set("currentUnlockedAreas", defaultVisitedZones);
+defaultNewPlayerArgs.set("currentDayCount", 1);
+
+function startGameLoop (gameState = defaultNewPlayerArgs) {
+	let playerStatus = new Map();
+	playerStatus = defaultNewPlayerArgs;
+	initializeGameWindow();
+	console.log(playerStatus);
+}
+
+function initializeGameWindow () {
+	let gameWindowDiv = document.createElement("div");
+	let gameWindowForm = document.createElement("form");
+	let gameWindowText = document.createElement("p");
+	gameWindowDiv.setAttribute("id", "game-window");
+	gameWindowForm.setAttribute("id", "game-form");
+	gameWindowText.setAttribute("id", "game-text");
+	document.body.appendChild(gameWindowDiv);
+	gameWindowDiv.appendChild(gameWindowText);
+	gameWindowDiv.appendChild(gameWindowForm);
+}
+
+function addButton (buttonText, buttonType, buttonFunction) {
+	let newButton = document.createElement("button");
+	newButton.setAttribute("type", buttonType);
+	newButton.setAttribute("onclick", buttonFunction);
+	let newButtonText = document.createTextNode(buttonText);
+	let gameWindowDiv = document.getElementById("game-window");
+	newButton.appendChild(newButtonText);
+	gameWindowDiv.appendChild(newButton);
+}
+
+function startDay (startZone) {
+	let currentZone = "Home";
+	addButton("Go Out", "button", "testAlert('test')");
+}
+
+function testAlert(val) {
+	alert(val);
+}
+
+// function initialText(zone) {
+// 	let gameWindowDiv = document.getElementById("gameWindow");
+// 	let description = document.createElement("p");
+// 	let inputForm = document.createElement("form");
+// 	let radioInput = document.createElement("input");
+// 	radioInput.setAttribute("type", "radio");
+// 	radioInput.setAttribute("name", "radio");
+// 	radioInput.setAttribute("value", "radio");
+// 	let textArea = document.createTextNode("Go Out");
+// 	let descText = document.createTextNode("You wake up. You are at " + zone + ". What will you do?");
+// 	description.appendChild(descText);
+// 	gameWindowDiv.appendChild(description);
+// 	gameWindowDiv.appendChild(inputForm);
+// 	inputForm.appendChild(radioInput);
+// 	inputForm.appendChild(textArea);
+// }
+
+startGameLoop();
+
+// get current location, default is home
+// prompt to travel or stay and grind (grind option not available from home location)
+// once travelling or staying, begin incrementing hours timer
+// each increment, roll D12 to get encounter chance
+// if encounter is triggered, roll D20 to get encounter from location's map
+// perform combat loop or other encounter logic
+// if combat encounter, restore player health afterwards
+// once location increment ends, prompt to either stay or travel
+// once hours timer hits 8, prompt reminder for 4 hours remaining
+// once hours timer hits 12, prompt travel or stay, travel locations within 4 hours travel time
+// once resting, check if player is home or not
+// if home, increment day counter and reset hour counter
+// if not home, roll D20 to get encounter from location's map, and do not allow player to run
+// once encounter ends, increment day counter and reset hour counter
