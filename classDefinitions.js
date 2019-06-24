471e7999ff890ebb87e2e0ca3f7df99356ca30b6
wjb1114 <kbd1114@gmail.com>
2019-06-24 11:55:16 -0500

    Joined JS files, added logic for selecting path and rolling on encounter tables.

--- classDefinitions.js
+++ classDefinitions.js
@@ -1,7 +1,90 @@
-let zonePathOne =["Mountain1", "Mountain2", "Mountain3", "Mountain4", "Mountain5"];
-let zzonePathTwo =["Island1", "Island2", "Island3", "Island4", "Island5"];
-let zzonePathThree =["Plains1", "Plains2", "Plains3", "Plains4", "Plains5"];
-let zzonePathFour =["Swamp1", "Swamp2", "Swamp3", "Swamp4", "Swamp5"];
-let zzonePathFive =["Forest1", "Forest2", "Forest3", "Forest4", "Forest5"];
-let zzonePaths = [zonePathOne, zonePathTwo, zonePathThree, zonePathFour, zonePathFive];
-let zzoneWeb = ["Home", zonePaths, "Castle"];
+let Zone = class {
+	constructor (name, encounterTable, timeToTravel) {
+		this.name = name;
+		this.encounterTable = encounterTable;
+		this.timeToTravel = timeToTravel;
+	}
+}
+let Enemy = class {
+	constructor (name, strength, accuracy, maxHealth, defence, specialMoveName, specialMoveDamage, expGiven) {
+		this.name = name;
+		this.strength = strength;
+		this.accuracy = accuracy;
+		this.maxHealth = maxHealth;
+		this.defence = defence;
+		this.specialMoveName = specialMoveName;
+		this.specialMoveDamage = specialMoveDamage;
+		this.expGiven = expGiven;
+	}
+}
+
+let CombatEncounter = class {
+	constructor (enemy) {
+		this.enemy = enemy;
+	}
+}
+
+let NonCombatEncounter = class {
+	constructor (textDescription, statToAffect, affectNumber) {
+		this.textDescription = textDescription;
+		this.statToAffect = statToAffect;
+		this.affectNumber = affectNumber;
+	}
+}
+let wolfEnemy = new Enemy ("Wolf", 10, 10, 150, 10, "Bite", 20, 25);
+
+let wolfEncounter = new CombatEncounter(wolfEnemy);
+let rockSlideEncounter = new NonCombatEncounter("You are hit by a rockslide!", currentHealth, -100);
+let emptyEncounter = new NonCombatEncounter("You weren't attacked and manage to gain some health back.", currentHealth, 20);
+
+let testTable = [wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, rockSlideEncounter, rockSlideEncounter, rockSlideEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter];
+let emptyTale = [emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter];
+
+let mountainTables = [testTable, testTable, testTable, testTable, testTable];
+let islandTables = [testTable, testTable, testTable, testTable, testTable];
+let plainsTables = [testTable, testTable, testTable, testTable, testTable];
+let swampTables = [testTable, testTable, testTable, testTable, testTable];
+let forestTables = [testTable, testTable, testTable, testTable, testTable];
+
+let homeZone = new Zone ("Home", emptyTable, 0);
+
+let mountainZoneOne = new Zone ("Mountain1", mountainTables[0], 1);
+let mountainZoneTwo = new Zone ("Mountain2", mountainTables[1], 2);
+let mountainZoneThree = new Zone ("Mountain3", mountainTables[2], 3);
+let mountainZoneFour = new Zone ("Mountain4", mountainTables[3], 4);
+let mountainZoneFive = new Zone ("Mountain5", mountainTables[4], 5);
+
+let islandZoneOne = new Zone ("Island1", islandTables[0], 1);
+let islandZoneTwo = new Zone ("Island2", islandTables[1], 2);
+let islandZoneThree = new Zone ("Island3", islandTables[2], 3);
+let islandZoneFour = new Zone ("Island4", islandTables[3], 4);
+let islandZoneFive = new Zone ("Island5", islandTables[4], 5);
+
+let plainsZoneOne = new Zone ("Plains1", plainsTables[0], 1);
+let plainsZoneTwo = new Zone ("Plains2", plainsTables[1], 2);
+let plainsZoneThree = new Zone ("Plains3", plainsTables[2], 3);
+let plainsZoneFour = new Zone ("Plains4", plainsTables[3], 4);
+let plainsZoneFive = new Zone ("Plains5", plainsTables[4], 5);
+
+let swampZoneOne = new Zone ("Swamp1", swampTables[0], 1);
+let swampZoneTwo = new Zone ("Swamp2", swampTables[1], 2);
+let swampZoneThree = new Zone ("Swamp3", swampTables[2], 3);
+let swampZoneFour = new Zone ("Swamp4", swampTables[3], 4);
+let swampZoneFive = new Zone ("Swamp5", swampTables[4], 5);
+
+let forestZoneOne = new Zone ("Forest1", forestTables[0], 1);
+let forestZoneTwo = new Zone ("Forest2", forestTables[1], 2);
+let forestZoneThree = new Zone ("Forest3", forestTables[2], 3);
+let forestZoneFour = new Zone ("Forest4", forestTables[3], 4);
+let forestZoneFive = new Zone ("Forest5", forestTables[4], 5);
+
+let finalZone = new Zone ("Castle", finalEncounterTable, 8);
+
+let zonePathOne = [homeZone, mountainZoneOne, mountainZoneTwo, mountainZoneThree, mountainZoneFour, mountainZoneFive, finalZone];
+let zonePathTwo = [homeZone, islandZoneOne, islandZoneTwo, islandZoneThree, islandZoneFour, islandZoneFive, finalZone];
+let zonePathThree = [homeZone, plainsZoneOne, plainsZoneTwo, plainsZoneThree, plainsZoneFour, plainsZoneFive, finalZone];
+let zonePathFour = [homeZone, swampZoneOne, swampZoneTwo, swampZoneThree, swampZoneFour, swampZoneFive, finalZone];
+let zonePathFive = [homeZone, forestZoneOne, forestZoneTwo, forestZoneThree, forestZoneFour, forestZoneFive, finalZone];
+let zonePaths = [zonePathOne, zonePathTwo, zonePathThree, zonePathFour, zonePathFive];
+
+let globalArgs = [zonePaths];
--- diceGame.js
+++ diceGame.js
@@ -1,5 +1,5 @@
 "use strict";
-// currentLevel, currentXP, str, acc, hp, def, unlockedAreas, dayCounter
+
 let defaultVisitedZones = ["Home"];
 let defaultNewPlayerArgs = new Map();
 defaultNewPlayerArgs.set("currentLevel", 1);
@@ -10,10 +10,174 @@ defaultNewPlayerArgs.set("currentMaxHealth", 1000);
 defaultNewPlayerArgs.set("currentDefense", 20);
 defaultNewPlayerArgs.set("currentUnlockedAreas", defaultVisitedZones);
 defaultNewPlayerArgs.set("currentDayCount", 1);
+defaultNewPlayerArgs.set("currentHealth", defaultNewPlayerArgs.currentMaxHealth);
+
+let playerStatus = new Map();
+playerStatus = defaultNewPlayerArgs;
+
+let currentPath;
+let currentZone;
+let currentHealth;
+let timeCounter;
+
+let wolfEncounter;
+let rockSlideEncounter;
+let emptyEncounter;
+
+let testTable = [];
+let emptyTable = [];
+let finalEncounterTable = [];
+
+let mountainTables = [];
+let islandTables = [];
+let plainsTables = [];
+let swampTables = [];
+let forestTables = [];
+
+let homeZone;
+
+let mountainZoneOne;
+let mountainZoneTwo;
+let mountainZoneThree;
+let mountainZoneFour;
+let mountainZoneFive;
+
+let islandZoneOne;
+let islandZoneTwo;
+let islandZoneThree;
+let islandZoneFour;
+let islandZoneFive;
+
+let plainsZoneOne;
+let plainsZoneTwo;
+let plainsZoneThree;
+let plainsZoneFour;
+let plainsZoneFive;
+
+let swampZoneOne;
+let swampZoneTwo;
+let swampZoneThree;
+let swampZoneFour;
+let swampZoneFive;
+
+let forestZoneOne;
+let forestZoneTwo;
+let forestZoneThree;
+let forestZoneFour;
+let forestZoneFive;
+
+let finalZone;
+
+let zonePathOne;
+let zonePathTwo;
+let zonePathThree;
+let zonePathFour;
+let zonePathFive;
+let zonePaths;
+
+let currentEnemy;
+let currentEnemyHealth;
+
+let Zone = class {
+	constructor (name, encounterTable, timeToTravel, chanceOfEncounter) {
+		this.name = name;
+		this.encounterTable = encounterTable;
+		this.timeToTravel = timeToTravel;
+		this.chanceOfEncounter = chanceOfEncounter;
+	}
+}
+let Enemy = class {
+	constructor (name, pluralName, strength, accuracy, maxHealth, defence, specialMoveName, specialMoveDamage, expGiven) {
+		this.name = name;
+		this.pluralName = pluralName;
+		this.strength = strength;
+		this.accuracy = accuracy;
+		this.maxHealth = maxHealth;
+		this.defence = defence;
+		this.specialMoveName = specialMoveName;
+		this.specialMoveDamage = specialMoveDamage;
+		this.expGiven = expGiven;
+	}
+}
+
+let CombatEncounter = class {
+	constructor (enemy) {
+		this.enemy = enemy;
+	}
+}
+
+let NonCombatEncounter = class {
+	constructor (textDescription, statToAffect, affectNumber) {
+		this.textDescription = textDescription;
+		this.statToAffect = statToAffect;
+		this.affectNumber = affectNumber;
+	}
+}
+let wolfEnemy = new Enemy ("Wolf", "Wolves", 10, 10, 150, 10, "Bite", 20, 25);
+
+function initEncounters() {
+	wolfEncounter = new CombatEncounter(wolfEnemy);
+	rockSlideEncounter = new NonCombatEncounter("You are hit by a rockslide!", currentHealth, -100);
+	emptyEncounter = new NonCombatEncounter("You weren't attacked and manage to gain some health back.", currentHealth, 20);
+
+	testTable = [wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, wolfEncounter, rockSlideEncounter, rockSlideEncounter, rockSlideEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter];
+	emptyTable = [emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter];
+	finalEncounterTable = [emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter, emptyEncounter];
+
+	mountainTables = [testTable, testTable, testTable, testTable, testTable];
+	islandTables = [testTable, testTable, testTable, testTable, testTable];
+	plainsTables = [testTable, testTable, testTable, testTable, testTable];
+	swampTables = [testTable, testTable, testTable, testTable, testTable];
+	forestTables = [testTable, testTable, testTable, testTable, testTable];
+
+	homeZone = new Zone ("Home", emptyTable, 0, 0);
+
+	mountainZoneOne = new Zone ("Mountain1", mountainTables[0], 1, 3);
+	mountainZoneTwo = new Zone ("Mountain2", mountainTables[1], 2, 4);
+	mountainZoneThree = new Zone ("Mountain3", mountainTables[2], 3, 5);
+	mountainZoneFour = new Zone ("Mountain4", mountainTables[3], 4, 8);
+	mountainZoneFive = new Zone ("Mountain5", mountainTables[4], 5, 9);
+
+	islandZoneOne = new Zone ("Island1", islandTables[0], 1, 3);
+	islandZoneTwo = new Zone ("Island2", islandTables[1], 2, 4);
+	islandZoneThree = new Zone ("Island3", islandTables[2], 3, 5);
+	islandZoneFour = new Zone ("Island4", islandTables[3], 4, 8);
+	islandZoneFive = new Zone ("Island5", islandTables[4], 5, 9);
+
+	plainsZoneOne = new Zone ("Plains1", plainsTables[0], 1, 3);
+	plainsZoneTwo = new Zone ("Plains2", plainsTables[1], 2, 4);
+	plainsZoneThree = new Zone ("Plains3", plainsTables[2], 3, 5);
+	plainsZoneFour = new Zone ("Plains4", plainsTables[3], 4, 8);
+	plainsZoneFive = new Zone ("Plains5", plainsTables[4], 5, 9);
+
+	swampZoneOne = new Zone ("Swamp1", swampTables[0], 1, 3);
+	swampZoneTwo = new Zone ("Swamp2", swampTables[1], 2, 4);
+	swampZoneThree = new Zone ("Swamp3", swampTables[2], 3, 5);
+	swampZoneFour = new Zone ("Swamp4", swampTables[3], 4, 8);
+	swampZoneFive = new Zone ("Swamp5", swampTables[4], 5, 9);
+
+	forestZoneOne = new Zone ("Forest1", forestTables[0], 1, 3);
+	forestZoneTwo = new Zone ("Forest2", forestTables[1], 2, 4);
+	forestZoneThree = new Zone ("Forest3", forestTables[2], 3, 5);
+	forestZoneFour = new Zone ("Forest4", forestTables[3], 4, 8);
+	forestZoneFive = new Zone ("Forest5", forestTables[4], 5, 9);
+
+	finalZone = new Zone ("Castle", finalEncounterTable, 8, 12);
+
+	zonePathOne = [homeZone, mountainZoneOne, mountainZoneTwo, mountainZoneThree, mountainZoneFour, mountainZoneFive, finalZone];
+	zonePathTwo = [homeZone, islandZoneOne, islandZoneTwo, islandZoneThree, islandZoneFour, islandZoneFive, finalZone];
+	zonePathThree = [homeZone, plainsZoneOne, plainsZoneTwo, plainsZoneThree, plainsZoneFour, plainsZoneFive, finalZone];
+	zonePathFour = [homeZone, swampZoneOne, swampZoneTwo, swampZoneThree, swampZoneFour, swampZoneFive, finalZone];
+	zonePathFive = [homeZone, forestZoneOne, forestZoneTwo, forestZoneThree, forestZoneFour, forestZoneFive, finalZone];
+	zonePaths = [zonePathOne, zonePathTwo, zonePathThree, zonePathFour, zonePathFive];
+}
+
+// currentLevel, currentXP, str, acc, hp, def, unlockedAreas, dayCounter
 
 function startGameLoop (gameState = defaultNewPlayerArgs) {
-	let playerStatus = new Map();
-	playerStatus = defaultNewPlayerArgs;
+	initEncounters();
+	currentZone = homeZone;
+	currentPath = zonePathOne;
 	initializeGameWindow();
 	startDay();
 	//console.log(playerStatus);
@@ -45,8 +209,13 @@ function addButton (buttonText, buttonType, buttonFunction, buttonClass) {
 function startDay (startZone) {
 	let textArea = document.getElementById("game-text");
 	textArea.innerHTML = "Test text";
-	addButton("Go Out", "button", "testAlert('leave')", "game-elements");
-	addButton("Stay and Train", "button", "testAlert('stay')", "game-elements");
+	addButton("Go Out", "button", "selectArea()", "game-elements");
+	if (currentZone !== homeZone) {
+		addButton("Stay and Train", "button", "testAlert('stay')", "game-elements");
+	}
+	currentHealth = playerStatus.maxHealth;
+	timeCounter = 0;
+	initEncounters();
 }
 
 function testAlert(val) {
@@ -63,22 +232,144 @@ function removeElementsByClassName (className) {
 	}
 }
 
-// function initialText(zone) {
-// 	let gameWindowDiv = document.getElementById("gameWindow");
-// 	let description = document.createElement("p");
-// 	let inputForm = document.createElement("form");
-// 	let radioInput = document.createElement("input");
-// 	radioInput.setAttribute("type", "radio");
-// 	radioInput.setAttribute("name", "radio");
-// 	radioInput.setAttribute("value", "radio");
-// 	let textArea = document.createTextNode("Go Out");
-// 	let descText = document.createTextNode("You wake up. You are at " + zone + ". What will you do?");
-// 	description.appendChild(descText);
-// 	gameWindowDiv.appendChild(description);
-// 	gameWindowDiv.appendChild(inputForm);
-// 	inputForm.appendChild(radioInput);
-// 	inputForm.appendChild(textArea);
-// }
+function selectArea() {
+	removeElementsByClassName ("game-elements");
+	let textArea = document.getElementById("game-text");
+	textArea.innerHTML = "Which area would you like to explore?";
+	addButton("Mountains", "button", "selectPath('mountain')", "game-elements");
+	addButton("Islands", "button", "selectPath('island')", "game-elements");
+	addButton("Plains", "button", "selectPath('plain')", "game-elements");
+	addButton("Swamp", "button", "selectPath('swamp')", "game-elements");
+	addButton("Forest", "button", "selectPath('forest')", "game-elements");
+}
+
+function selectPath (pathToSelect) {
+	removeElementsByClassName ("game-elements");
+	let currentVisitedZone = currentZone;
+	let textArea = document.getElementById("game-text");
+	textArea.innerHTML = "You start heading towards the " + pathToSelect;
+	addButton("Okay", "button", "travelPath('" + pathToSelect + "')", "game-elements");
+}
+
+function travelPath(currentPathOption) {
+	if (currentZone === homeZone) {
+		if (currentPathOption === 'mountain') {
+			currentPath = zonePathOne;
+		}
+		else if (currentPathOption === 'island') {
+			currentPath = zonePathTwo;
+		}
+		else if (currentPathOption === 'plain') {
+			currentPath = zonePathThree;
+		}
+		else if (currentPathOption === 'swamp') {
+			currentPath = zonePathFour;
+		}
+		else if (currentPathOption === 'forest') {
+			currentPath = zonePathFive;
+		}
+	}
+	let pathIndex = currentPath.indexOf(currentZone);
+	beginTravelTime (currentPath[pathIndex + 1]);
+}
+
+function beginTravelTime(areaGoal) {
+	currentZone = areaGoal;
+	let travelCounter = 0;
+	while (travelCounter < areaGoal.timeToTravel) {
+		if (rollDie(12) <= currentZone.chanceOfEncounter) {
+			initEncounters();
+			performEncounter(currentZone.encounterTable[rollDie(20)]);
+		}
+		timeCounter++;
+		travelCounter++;
+	}
+}
+
+function performEncounter(encounter) {
+	if (encounter.constructor.name === "CombatEncounter")	{
+		//name, strength, accuracy, maxHealth, defence, specialMoveName, specialMoveDamage, expGiven
+		removeElementsByClassName ("game-elements");
+		let textArea = document.getElementById("game-text");
+		currentEnemy = new Enemy(encounter.enemy.name, encounter.enemy.pluralName, encounter.enemy.strength, encounter.enemy.accuracy, encounter.enemy.maxHealth, encounter.enemy.defence, encounter.enemy.specialMoveName, encounter.enemy.specialMoveDamage, encounter.enemy.expGiven);
+		let numEnemies = rollDie(4);
+		if (numEnemies > 1) {
+			currentEnemy.maxHealth *= numEnemies;
+			currentEnemy.expGiven *= numEnemies;
+			currentEnemyHealth = currentEnemy.maxHealth;
+			alert("You are attacked by " + numEnemies + " " + currentEnemy.namePlural + "!");
+			textArea.innerHTML = "The " + numEnemies + " " + currentEnemy.pluralName + " are staring you down.";
+		}
+		else {
+			currentEnemyHealth = currentEnemy.maxHealth;
+			alert("You are attacked by a " + currentEnemyName + "!");
+			textArea.innerHTML = "The " + currentEnemy.name + " is staring you down.";
+		}
+
+		addButton("Attack", "button", "processDamage(" + playerStatus.strength + ")", "game-elements");
+		addButton("Special Attack", "button", "specialAttack(" + playerStatus.strength + ")", "game-elements");
+		addButton("Heal", "button", "healEntity(" + playerStatus.strength + ")", "game-elements");
+		addButton("Run Away", "button", "runFromBattle(" + playerStatus.strength + ")", "game-elements");
+	}
+	else if (encounter.constructor.name === "NonCombatEncounter") {
+		//textDescription, statToAffect, affectNumber
+	}
+}
+
+function playerAttack () {
+	addButton("Attack", "button", "processDamage(" + playerStatus.strength + ")", "game-elements");
+	addButton("Special Attack", "button", "specialAttack(" + playerStatus.strength + ")", "game-elements");
+	addButton("Heal", "button", "healEntity(" + playerStatus.strength + ")", "game-elements");
+	addButton("Run Away", "button", "runFromBattle(" + playerStatus.strength + ")", "game-elements");
+}
+
+function enemyAttack () {
+
+}
+
+function rollDie (numSides) {
+	return Math.floor(Math.random() * numSides) + 1;
+}
+
+function processDamage(damageVal, attackingEntity, receivingEntity) {
+	removeElementsByClassName ("game-elements");
+	let textArea = document.getElementById("game-text");
+	let accuracyRollOne = rollDie(8);
+	let accuracyRollTwo = rollDie(8);
+	let largerRoll;
+	if (accuracyRollOne > accuracyRollTwo) {
+		largerRoll = accuracyRollOne;
+	}
+	else {
+		largerRoll = accuracyRollTwo;
+	}
+
+	if (largerRoll === 8) {
+		//automatically hits
+	}
+	else if (accuracyRoll * attackingEntity.accuracy > 125) {
+		//hits
+	}
+	else {
+		//misses
+	}
+}
+
+function specialAttack(damageVal, attackingEntity, receivingEntity) {
+	for (let i = 0; i < rolLDie(4); i++) {
+		processDamage(damageVal, attackingEntity, receivingEntity);
+	}
+}
+
+function healEntity(healVal) {
+	removeElementsByClassName ("game-elements");
+	let textArea = document.getElementById("game-text");
+}
+
+function runFromBattle() {
+	removeElementsByClassName ("game-elements");
+	let textArea = document.getElementById("game-text");
+}
 
 startGameLoop();
 
